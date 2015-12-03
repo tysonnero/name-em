e5ad2ce709c17377dc808de30bdb540e4dd62088
Tyson Nero <tyson@tnero.com>
2015-12-02 23:08:10 -0500

    Added alerts Added validation Adjusted scroll layout Implemented remove button

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,9 +1,9 @@
 angular.module('starter.controllers', [])
 
-  .controller('DashCtrl', function ($scope) {
+  .controller('DashCtrl', function($scope) {
   })
 
-  .controller('ChatsCtrl', function ($scope, Chats) {
+  .controller('ChatsCtrl', function($scope, Chats) {
     // With the new view caching in Ionic, Controllers are only called
     // when they are recreated or on app start, instead of every page change.
     // To listen for when this page is active (for example, to refresh data),
@@ -13,16 +13,16 @@ angular.module('starter.controllers', [])
     //});
 
     $scope.chats = Chats.all();
-    $scope.remove = function (chat) {
+    $scope.remove = function(chat) {
       Chats.remove(chat);
     };
   })
 
-  .controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
+  .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
     $scope.chat = Chats.get($stateParams.chatId);
   })
 
-  .controller('AccountCtrl', function ($scope) {
+  .controller('AccountCtrl', function($scope) {
     $scope.settings = {
       enableFriends: true
     };
@@ -30,20 +30,25 @@ angular.module('starter.controllers', [])
 
   .controller('StatesCtrl', StatesCtrl);
 
-function StatesCtrl(StatesService) {
+function StatesCtrl(StatesService, $ionicContentBanner) {
+  this.$ionicContentBanner = $ionicContentBanner;
   this.states = StatesService.get();
-  this.available = this.states;
+  this.available = angular.copy(this.states);
   this.matched = [];
 }
 
-StatesCtrl.prototype.findState = function (searchText) {
+StatesCtrl.prototype.add = function(searchText) {
 
-  if (!searchText) return;
+  // TODO:
+  // Abstract this method out to call a sub-routine that returns a promise
+  // Use the promise to display alerts
+
+  if (!searchText) return this.showAlert(['Missing state'], 'error');
 
   // Do a fuzzy search to accommodate for misspellings
   var f = new Fuse(this.available);
   var result = f.search(searchText, {threshold: 0.5, distance: 0});
-  if (!result.length) return;
+  if (!result.length) return this.showAlert([searchText + ' not found'], 'error');
 
   // TODO:
   // If score is exact match, proceed
@@ -54,10 +59,10 @@ StatesCtrl.prototype.findState = function (searchText) {
   var match = this.available[matchedIndex];
 
   // First letter of match should equal input
-  if (match.toLowerCase().charAt(0) !== searchText.toLowerCase().charAt(0)) return;
+  if (match.toLowerCase().charAt(0) !== searchText.toLowerCase().charAt(0)) return this.showAlert([searchText + ' not found'], 'error');
 
   // Input should be at least half the length of match
-  if (searchText.length < (Math.floor(match.length / 2))) return;
+  if (searchText.length < (Math.floor(match.length / 2))) return this.showAlert([searchText + ' not found'], 'error');
 
   // Add searchText to the matched array
   this.matched.push(match);
@@ -65,7 +70,31 @@ StatesCtrl.prototype.findState = function (searchText) {
   // Remove searchText from the available list
   this.available.splice(matchedIndex, 1);
 
+  this.showAlert([match + ' added to list'], 'info');
+
   // Reset form
   this.searchText = '';
   this.searchForm.$setPristine();
+};
+
+StatesCtrl.prototype.remove = function(item) {
+
+  var matchedIndex = this.matched.indexOf(item);
+  var originalIndex = this.states.indexOf(item);
+
+  // Remove from matched list
+  this.matched.splice(matchedIndex, 1);
+
+  // Add back to available list
+  this.available.splice(originalIndex, 0, item);
+
+  // TODO: Confirm removal
+};
+
+StatesCtrl.prototype.showAlert = function(text, type) {
+  this.$ionicContentBanner.show({
+    text: text,
+    autoClose: 5000,
+    type: type
+  });
 };
